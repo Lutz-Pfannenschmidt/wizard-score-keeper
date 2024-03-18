1305470b2ddefb8632890e4b447b4602904ed664
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2024-03-18 09:08:06 +0100

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lutz-pfannenschmidt.github.io',
+  base: '/wizard-score-keeper',
   integrations: [tailwind()]
 });
